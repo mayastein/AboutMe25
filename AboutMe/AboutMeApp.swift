e0895b7d293330cbf2c4edb5a2da4086d257351f
Maya Stein <mayarosestein@gmail.com>
2025-04-08 16:34:38 -0400

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Maya Stein on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMeTests/AboutMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeTests.swift
+//  AboutMeTests
+//
+//  Created by Maya Stein on 4/8/25.
+//
+
+import Testing
+@testable import AboutMe
+
+struct AboutMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
