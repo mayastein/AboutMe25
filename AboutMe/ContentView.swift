5c15bbd7395ac8dc66c0cbf7d880d23f68eeb64e
Maya Stein <mayarosestein@gmail.com>
2025-04-09 10:18:15 -0400

    Added a list of 3 facts, a title inside a capsule, and dividers/other styling changes

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack {
+                Divider()
+                Text("All About Me Project 2025")
+                    .font(.title)
+                    .fontWeight(.thin)
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.top)
+                ZStack {
+                    Capsule()
+                        .padding(0.0)
+                        .frame(width: 300.0, height: 80.0)
+                        .foregroundColor(Color.purple)
+                    Text("Maya Rose Stein")
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color.white)
+                }
+                Divider()
+                Text("Some fun facts about me :)")
+                List {
+                    Text("My favorite artist is Noah Kahan, closely followed by Taylor Swift")
+                    Text("I have 2 sisters, and we each have different hair colirs")
+                    Text("I love animals, especially my dog, Sammy")
+                }
+                .frame(width: /*@START_MENU_TOKEN@*/350.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+
+            }
+            .padding(10.0)
+
         }
-        .padding()
+
     }
 }
 
